bfa7460ae43ead802af5a0cbee05ddf3dd7144f9
DEADSEC-fr <tygaib09@gmail.com>
2024-09-12 10:16:47 +0200

    fonctions test fin

--- TP1/test.py
+++ TP1/test.py
@@ -8,3 +8,11 @@ def test_1( ) :
 def test_2( ) :
 	assert f.puissance(-2, 3) == -8
 	assert f.puissance(2, -3.) == (1.0 / 8.0)
+	
+def test_3( ) :
+	with pytest.raises(Exception) as e_info:
+		x = f.puissance(0, -5)
+
+def test_4( ) :
+	assert f.puissance(0, 0) == 1
+	assert f.puissance(0, 1) == 0
