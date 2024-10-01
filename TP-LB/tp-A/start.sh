dcdfcab73e3eeaaa01a670cb8d8b747307efb9d3
Ghoestrider75001 <tygaib09@gmail.com>
2024-10-01 11:34:25 +0200

    Starting TP-load Balancing

---
+++ TP-LB/tp-A/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Construire l'image im-nginx-lb
+docker build -t im-nginx-lb -f Dockerfile .
+
+# Créer les sous-dossiers "shared1" et "shared2" et placer les fichiers index.html
+mkdir shared1 shared2
+echo "<h1>Hello 1</h1>" > shared1/index.html
+echo "<h1>Hello 2</h1>" > shared2/index.html
+
+# Lancer les conteneurs nginx1 et nginx2 en mode détaché avec les ports mappés et le bind mount
+#nignx1
+docker run -d \
+    --name nginx1 \
+    -p 81:80 \
+    -v $(pwd)/shared1:/usr/share/nginx/html nginx
+
+#nignx2
+docker run -d \
+    --name nginx2 \
+    -p 82:80 \
+    -v $(pwd)/shared2:/usr/share/nginx/html nginx
+
+# Lancer le conteneur basé sur l'image im-nginx-lb
+docker run -d -p 83:80 --name nginx-lb im-nginx-lb
