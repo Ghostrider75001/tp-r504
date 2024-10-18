1bceab78a38c5b4ed74f0e873548aa8d30ef7ed3
Ghoestrider75001 <tygaib09@gmail.com>
2024-10-18 13:38:38 +0200

    Le client ne renvoie pas la str

--- TP9/ServeurUDP.java
+++ TP9/ServeurUDP.java
@@ -3,28 +3,29 @@ import java.net.*;
 
 public class ServeurUDP 
 {
-    public static void main(String[] args)
-    {
-        try 
-        {
-            DatagramSocket sock = new DatagramSocket(1234);
-            while (true) 
-            {
-                System.out.println("- Waiting Data");
-                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-                sock.receive(packet);
-                String str = new String(packet.getData());
-                System.out.println( "str= " + str);
-                
-                byte[] data = str.getBytes ( );
-                DatagramPacket packet2 = new DatagramPacket(data, data.length, packet.getAddress(), 1234);
-                sock.send(packet2);
-            }
-        } 
-        catch (Exception ex) 
-        {
-            System.out.println ( "Erreur!" );
-        }
-        
-    }
+	public static void main(String[] args)
+	{
+		while (true) 
+		{
+			try 
+			{
+				DatagramSocket sock = new DatagramSocket(1234);
+				System.out.println("- Waiting Data");
+				DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+				sock.receive(packet);
+				String str = new String(packet.getData());
+				System.out.println( "str= " + str);
+
+				byte[] data = str.getBytes ( );
+				DatagramPacket packet2 = new DatagramPacket(data, data.length, packet.getAddress(), 1234);
+				sock.send(packet2);
+				sock.close();
+			}
+			catch (Exception ex) 
+			{
+				System.out.println ( "Erreur!" );
+			}
+
+		}
+	}
 }
